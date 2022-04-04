a961da9372dfb226f9f3f609e4840ca4f5944b9b
ianheuser <ian.mccarthy@anheuser-busch.com>
2022-04-04 13:49:02 -0500

    Step Two, refactoring our static page into a set of reusable components
    Step Two, refactoring our static page into a set of reusable components

--- sveltekit-skeleton/src/lib/OurBeers.svelte
+++ sveltekit-skeleton/src/lib/OurBeers.svelte
@@ -1,37 +1,29 @@
 <script>
-    export let sectionTitle = 'Our Beers';
+    export let sectionTitle = 'Beers';
     import Beer from "./Beer.svelte";
+     
+    export let beers;
+
 </script>
 
-
 <div class="space section ourBeers">
-    <h2>{ sectionTitle } </h2>
-    <div class="space container beers">
 
-        <Beer 
-            name="Michelob Ultra" 
-            blurb="This is a great blurb about Michelob" 
-            src="./img/profile_bottle_pure_gold.png" 
-            alt="A profile view image of Michelob Ultra" 
-        />
-        <Beer 
-            name="Michelob Ultra Pure Gold" 
-            blurb="USDA Certified Organic Beer" 
-            src="./img/profile_bottle_pure_gold.png" 
-            alt="A profile view image of Michelob Ultra Pure Gold" 
-        />
-        <Beer 
-            name="Michelob Ultra Amber Max" 
-            blurb="Taste the Impossible" 
-            src="./img/profile_can_amber.jpg" 
-            alt="A profile view image of Michelob Ultra Amber Max" 
-        />
-        <Beer 
-            name="Michelob Ultra Infusions" 
-            blurb="Beer meets exotic fruit" 
-            src="./img/profile_can_infusions.png" 
-            alt="A profile view image of Michelob Ultra Infusions Can" 
-        />
+    <h2>Our { sectionTitle } </h2>
+    
+    <div class="space container beers">
+      
+            {#each beers as beer}
+               
+                <Beer 
+                    name="{beer.name}"
+                    blurb="{beer.blurb}"
+                    src="{beer.src}"
+                    alt="{beer.alt}"
+                />
+            
+            {/each}
+
+
     </div>  
 </div>
 
--- sveltekit-skeleton/src/routes/index.svelte
+++ sveltekit-skeleton/src/routes/index.svelte
@@ -1,14 +1,48 @@
 <script>
     import Carousel from '$lib/Carousel.svelte';
-    import Navigation from '$lib/Navigation.svelte';
     import VideoSection from "$lib/videoSection.svelte";
     import OurBeers from "$lib/OurBeers.svelte";
     import RibbonAds from "$lib/RibbonAds.svelte"
+
+    let beers = [{
+            "name":"Michelob Ultra",
+            "blurb":"This blurb is coming from the beers object",
+            "src":"./img/profile_bottle_pure_gold.png",
+            "alt":"A profile view image of Michelob Ultra"
+        },{ 
+            "name":"Michelob Ultra Pure Gold",
+            "blurb":"USDA Certified Organic Beer",
+            "src":"./img/profile_bottle_pure_gold.png",
+            "alt":"A profile view image of Michelob Ultra Pure Gold"
+        },{ 
+            "name":"Michelob Ultra Amber Max",
+            "blurb":"Taste the Impossible",
+            "src":"./img/profile_can_amber.jpg",
+            "alt":"A profile view image of Michelob Ultra Amber Max"
+        },{ 
+            "name":"Michelob Ultra Infusions",
+            "blurb":"Beer meets exotic fruit",
+            "src":"./img/profile_can_infusions.png",
+            "alt":"A profile view image of Michelob Ultra Infusions Can"
+        }];
+
+    let seltzers = [{ 
+            "name":"Michelob Ultra Organic Seltzer",
+            "blurb":"Its only worth it if you enjoy it",
+            "src":"./img/profile_can_seltzer.png",
+            "alt":"A profile view image of Michelob Ultra Seltzer Can"
+        }];
 </script>
 
 <Carousel />
 <VideoSection />
-<OurBeers sectionTitle="Our Beers" />
-<OurBeers sectionTitle="Our Seltzers" />  
+<OurBeers 
+    sectionTitle="Beers"
+    beers={beers} 
+/>
+<OurBeers 
+    sectionTitle="Seltzers"
+    beers={seltzers} 
+/>  
 <RibbonAds /> 
        
